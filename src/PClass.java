bd436a2ce1a3a9ae0b0a5c176cc8dea4b99d34e5
vero6080 <vero6080@ISS2V2RKH2.scdsb.pri>
2019-01-11 14:24:57 -0500

    Added basic requirements for ISU

---
+++ src/PClass.java
@@ -0,0 +1,11 @@
+public class PClass {
+    /*
+    - 5 varied properties
+    - 2 constructors
+    - 2 final methods
+    - 1 abstract method
+    - 1 regualar method
+    - 1 static variable
+    - 1 static method
+    */
+}
